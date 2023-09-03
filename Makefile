e57bcb32f9c50da90f96dae3c7cabd451561b3a4
Ahmed Ait Lfdil <aait-lfd@c3r10p7.1337.ma>
2023-09-03 11:16:43 +0100

    init

---
+++ Makefile
@@ -0,0 +1,57 @@
+# Colors
+RED = \033[0;31m
+GREEN = \033[0;32m
+YELLOW = \033[0;33m
+END = \033[0m
+
+#Texture
+BOLD = \033[1m
+
+# Compiler and flags
+CC = cc
+CFLAGS = -Wall -Wextra -Werror #-fsanitize=address
+LDFLAGS =
+
+# Target name
+NAME = cub3D
+LIB = ./lib/lib.a
+
+# Source files
+LIB_SRC = $(wildcard ./lib/*.c ./src/**/*.c)
+SRC = $(wildcard ./src/*.c ./src/**/*.c)
+OBJ = $(SRC:.c=.o)
+
+all : $(NAME)
+
+
+$(LIB) : $(LIB_SRC)
+	@echo "$(GREEN)\n⌛ Compiling lib ...$(END)"
+	@make -sC ./lib
+	@make -sC ./lib/ clean
+
+$(NAME): $(SRC) $(LIB)
+	@echo "$(GREEN)⌛ Compiling $(NAME) ...$(END)"
+	@$(CC) $(CFLAGS) $(SRC) $(LIB) $(LDFLAGS) -o $(NAME)
+	@echo "$(GREEN)✅ $(NAME) compiled successfully\n$(END)"
+	@echo "To start the game, use: $(YELLOW)$(BOLD)make start$(END)"
+
+
+start :
+	clear
+	@./$(NAME)
+
+clean :
+	@echo "$(RED)\n⌛ removing object files ...$(END)"
+	@rm -rf $(OBJ)
+	@make -sC  ./lib clean
+	@echo "$(RED)🗑️  objects removed \n$(END)"
+
+fclean : clean
+	@echo "$(RED)\n⌛ removing $(NAME) ...$(END)"
+	@rm -rf $(NAME)
+	@make -sC  ./lib fclean
+	@echo "$(RED)🗑️  $(NAME) removed \n$(END)"
+
+re : fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/inc.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inc.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 18:12:30 by aait-lfd          #+#    #+#             */
+/*   Updated: 2023/09/01 18:29:42 by aait-lfd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INC_H
+# define INC_H
+
+// Text color macros
+# define COLOR_BLACK "\x1b[30m"
+# define COLOR_RED "\x1b[31m"
+# define COLOR_GREEN "\x1b[32m"
+# define COLOR_YELLOW "\x1b[33m"
+# define COLOR_BLUE "\x1b[34m"
+# define COLOR_MAGENTA "\x1b[35m"
+# define COLOR_CYAN "\x1b[36m"
+# define COLOR_WHITE "\x1b[37m"
+# define COLOR_RESET "\x1b[0m"
+
+// Text style macros
+# define TEXT_BOLD "\x1b[1m"
+# define RESET_ALL "\x1b[0m"
+
+# include <dirent.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
+/*---------*/
+# include "./lib.h"
+/*--------*/
+/*--------*/
+
+#endif

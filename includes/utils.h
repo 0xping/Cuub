9affc051bd3439419303c55b8fa4c8a5d597f662
Ahmed Ait Lfdil <aait-lfd@c3r9p2.1337.ma>
2023-09-13 00:19:17 +0100

    utils: add new utils

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 15:47:10 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/09/12 23:45:00 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/09/13 00:17:57 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ char	**readfile(char *filename);
 void	free_double_ptr(void **ptr);
 char	**split(char *str, char *dels);
 int		get_color_from_string(char *color);
+int	arg_to_int(const char *str);
+
 #endif

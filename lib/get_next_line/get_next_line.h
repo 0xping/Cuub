09a50fa7ab6989243ae8cb14f85c2010de171e8b
Ahmed Ait Lfdil <aait-lfd@c3r9p2.1337.ma>
2023-09-12 23:50:19 +0100

    fix: norm

--- lib/get_next_line/get_next_line.h
+++ lib/get_next_line/get_next_line.h
@@ -22,8 +22,8 @@
 # include <limits.h>
 # include <signal.h>
 # include <stdbool.h>
-# include <stdio.h>
 # include <stdint.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/types.h>
@@ -31,10 +31,9 @@
 # include <termios.h>
 # include <unistd.h>
 
-char					*ft_strjoin(char const *s1, char const *s2);
-char					*ft_strdup(const char *s1);
-char					*ft_substr(char const *s, unsigned int start,
-							size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ln_index(char *s);
 size_t	ft_strlen(const char *str);
 void	*ft_calloc(size_t count, size_t size);

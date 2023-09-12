73672747f468e9061a4d1c189ba1a33ae01790ea
Ahmed Ait Lfdil <aait-lfd@c3r9p2.1337.ma>
2023-09-13 00:03:19 +0100

    new error format

--- src/utils/ft_exit.c
+++ src/utils/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 18:36:52 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/09/12 22:59:46 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/09/12 23:57:00 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	ft_exit(char *msg, int err)
 {
+	ft_putstr_fd(COLOR_RED" Error\n    ", 2);
 	ft_putendl_fd(msg, 2);
 	exit(err);
 }
